5894fd65750e7e608c7a70f7aa9b9487e6166af2
Lukas Nocker <lukasnocker@gmail.com>
2022-05-21 23:48:48 +0200

    added basic Attack

---
+++ GameDevJam-Project/Assets/Skripts/PlayerCombat.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayer;
+
+    public Collider2D enemy;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Attack();
+        }
+    }
+
+   void Attack()
+   {
+        // play attack animation
+        Debug.Log("attacked");
+
+        // detect enemys in range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit" + enemy.name);
+        }
+   }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+}
