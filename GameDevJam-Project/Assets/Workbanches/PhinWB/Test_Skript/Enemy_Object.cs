d246f1f1769ce88522bf8d73d2dd980bc10342f3
PhineasJayden <phineas.jayden@outlook.de>
2022-05-22 08:37:13 +0200

    Enemy, Game Manager, Floating Text
    Enemy Logic - walking, chasing, making damage
    
    Floating Text Manager to display floating text
    
    Game Manager for storing and loading

---
+++ GameDevJam-Project/Assets/Workbanches/PhinWB/Test_Skript/Enemy_Object.cs
@@ -0,0 +1,166 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Object : MonoBehaviour
+{
+    //Public Fields
+    public int hitpoint = 10;
+    public int maxHitpoint = 10;
+    public float pushRecoverySpeed = 0.2f;
+
+    //Immunity
+    protected float immuneTime = 1.0f;
+    protected float lastImmune;
+
+    //Moving fields
+    protected CapsuleCollider2D capsuleCollider;
+    protected Vector3 moveDelta;
+    protected RaycastHit2D hit;
+    protected float ySpeed = 0.75f;
+    protected float xSpeed = 1.0f;
+
+    //Souls
+    public int soulsValue = 1;
+
+    //Logic
+    public float triggerLength = 1;
+    public float chaseLength = 5;
+    private bool chasing;
+    private bool collidingWithPlayer;
+    private Transform playerTransform;
+    private Vector3 startingPosition;
+
+    //Hitbox
+    public ContactFilter2D filter;
+    private CapsuleCollider2D hitbox;
+    private Collider2D[] hits = new Collider2D[10];
+
+    //push
+    protected Vector3 pushDirection;
+
+    //start
+    protected virtual void Start()
+    {
+            capsuleCollider = GetComponent<CapsuleCollider2D>();
+            playerTransform = GameManager.instance.player.transform;
+            startingPosition = transform.position;
+            hitbox = transform.GetChild(0).GetComponent<CapsuleCollider2D>();
+    }
+
+    //Enemy Moving
+
+    protected virtual void UpdateMotor(Vector3 input)
+    {
+        // Reset MoveDelta
+        moveDelta = new Vector3(input.x * xSpeed, input.y * ySpeed, 0);
+
+        //Swap Sprite direction, wehter you're going right or left
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        //add push vector, if any
+        moveDelta += pushDirection;
+
+        //reduc epush force every frame, based off recovery speed
+
+        pushDirection = Vector3.Lerp(pushDirection, Vector3.zero, pushRecoverySpeed);
+
+        // Make sure we can move in this direction, by casting a box tehre first, if the box returns null, we're free to move
+        hit = Physics2D.BoxCast(transform.position, capsuleCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Players", "Blocking"));
+        if (hit.collider == null)
+
+        {
+
+            // make this thing move!
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+
+        }
+        // Make sure we can move in this direction, by casting a box tehre first, if the box returns null, we're free to move
+        hit = Physics2D.BoxCast(transform.position, capsuleCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Players", "Blocking"));
+        if (hit.collider == null)
+
+        {
+
+            // make this thing move!
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+
+        }
+    }
+
+    //Chasing Mechanic
+    private void FixedUpdate()
+    {
+        //is the player in range?
+        if (Vector3.Distance(CharakterControllerTransform.position, startingPosition) < chaseLength)
+        {
+            if (Vector3.Distance(CharakterControllerTransform.position, startingPosition) < triggerLength)
+
+                chasing = true;
+
+
+            if (chasing)
+            {
+                if (!collidingWithPlayer)
+                {
+                    UpdateMotor((CharakterControllerTransform.position - transform.position).normalized);
+                }
+            }
+            else
+            {
+                UpdateMotor(startingPosition - transform.position);
+            }
+        }
+        else
+        {
+            UpdateMotor(startingPosition - transform.position);
+            chasing = false;
+        }
+
+        // Check for Overlaps
+        collidingWithPlayer = false;
+        capsuleCollider.OverlapCollider(filter, hits);
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i] == null)
+                continue;
+
+
+            if (hits[i].tag == "Player" && hits[i].name == "Player")
+            {
+                collidingWithPlayer = true;
+            }
+
+            //the array is not cleaned up, so we do it ourself
+            hits[i] = null;
+        }
+
+    }
+    // All Enemies can receive damage / die
+    protected virtual void ReceiveDamage(Damage dmg)
+    {
+        if (Time.time - lastImmune > immuneTime)
+        {
+            lastImmune = Time.time;
+            hitpoint -= dmg.damageAmount;
+            pushDirection = (transform.position - dmg.origin).normalized * dmg.pushForce;
+
+            GameManager.instance.ShowText(dmg.damageAmount.ToString(), 25, Color.red, transform.position, Vector3.up * 25, 0.5f);
+
+            if (hitpoint <= 0)
+            {
+                hitpoint = 0;
+                Death();
+            }
+        }
+    }
+
+    protected virtual void Death()
+    {
+        Destroy(gameObject);
+        GameManager.instance.souls += soulsValue;
+        GameManager.instance.ShowText("+" + soulsValue + " xp", 30, Color.magenta, transform.position, Vector3.up * 50, 3.0f);
+    }
+}
---
+++ GameDevJam-Project/Assets/Workbanches/PhinWB/Test_Skript/SkripteDungeon/NPC.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{ 
+public ContactFilter2D filter;
+private BoxCollider2D boxCollider;
+private Collider2D[] hits = new Collider2D[10];
+private float lastInteraction;
+private float cooldownNpc = 2.0f;
+private bool collided = false;
+
+    protected virtual void Start()
+{
+    boxCollider = GetComponent<BoxCollider2D>();
+}
+
+protected virtual void Update()
+{
+    // Collison work
+    boxCollider.OverlapCollider(filter, hits);
+    for (int i = 0; i < hits.Length; i++)
+    {
+        if (hits[i] == null)
+            continue;
+
+
+        OnCollide(hits[i]);
+
+        //teh array is not cleaned up, so we do it ourself
+        hits[i] = null;
+    }
+}
+    protected virtual void OnCollide(Collider2D coll)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Time.time - lastInteraction > cooldownNpc)
+            {
+                lastInteraction = Time.time;
+                GameManager.instance.ShowText("Hey! My Name is Avery. Who are you?", 15, Color.white, transform.position, Vector3.up * 20, 3.0f);
+
+            }   
+        }
+    }
+}
