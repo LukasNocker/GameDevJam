023a2c28e5d0b35b6eb9fc38de3a693d8a2107bf
PhineasJayden <phineas.jayden@outlook.de>
2022-05-25 21:52:06 +0200

    npc skript modified

--- GameDevJam-Project/Assets/Workbanches/PhinWB/Test_Skript/SkripteDungeon/NPC.cs
+++ GameDevJam-Project/Assets/Workbanches/PhinWB/Test_Skript/SkripteDungeon/NPC.cs
@@ -5,27 +5,29 @@ using UnityEngine.UI;
 
 public class NPC_dialog : MonoBehaviour
 {
-public string dialog;
-public Text dialogText;
-public GameObject dialogBox;
-public GameObject npcImage;
-public ContactFilter2D filter;
-private BoxCollider2D boxCollider;
-private Collider2D[] hits = new Collider2D[10];
-private float lastInteraction;
-private float cooldownNpc = 1.0f;
-public bool playerInRange;
+    public string dialog;
+    public Text dialogText;
+    public GameObject dialogBox;
+    public GameObject npcImage;
+    public ContactFilter2D filter;
+    private BoxCollider2D boxCollider;
+    private Collider2D[] hits = new Collider2D[10];
+    private float lastInteraction;
+    private float cooldownNpc = 1.0f;
+    public bool playerInRange;
 
 
 
 
     protected virtual void Start()
-{
-    boxCollider = GetComponent<BoxCollider2D>();
-}
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        dialogBox.SetActive(false);
+        playerInRange = false;
+    }
 
-protected virtual void Update()
-{
+    protected virtual void Update()
+    {
 
         //new Collider
         playerInRange = false;
@@ -45,37 +47,32 @@ protected virtual void Update()
             hits[i] = null;
         }
 
- 
-}
-    protected virtual void OnCollide(Collider2D coll)
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (playerInRange == true)
-                {
-                if (Time.time - lastInteraction > cooldownNpc)
-                    {
-                         lastInteraction = Time.time;
-                         if (this.dialogBox.activeInHierarchy)
-                         {
-                        this.dialogBox.SetActive(false);
 
-                         }
-                         else
-                         {
-                       this.dialogBox.SetActive(true);
-                        dialogText.text = dialog;
-                         }
-                }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+                this.dialogBox.SetActive(true);
+                dialogText.text = dialog;
+
             }
+
         }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-        {
-            playerInRange = false;
-            dialogBox.SetActive(false);
-        }
+            if (other.CompareTag("Player"))
+            {
+                playerInRange = false;
+                dialogBox.SetActive(false);
+            }
     }
+    
 }
