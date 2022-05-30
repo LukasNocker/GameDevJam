e5903f704ae474ce2add3cbf2c486f89dab079b9
Thomas Lepiarz <thomas.lepiarz@gmail.com>
2022-05-30 22:53:37 +0200

    Update Timer.cs
    TT

--- GameDevJam-Project/Assets/Skripts/Timer.cs
+++ GameDevJam-Project/Assets/Skripts/Timer.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+    public float currentTime;
+    private bool inOverworld;
     // Start is called before the first frame update
     void Start()
     {
-        
+        inOverworld = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (inOverworld)
+        {
+            currentTime -= 1 * Time.deltaTime;
+            GameManager.instance.souls = currentTime;
+            if (currentTime == 0)
+            {
+                inOverworld = false;
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Ghetto_2");
+            }
+        }
+    }
+
+    public void StartTimer()
+    {
+        inOverworld = true;
     }
 }
